abe2adb02cc1bbbf5592262bd79aa045e72f2d08
Dimitry <diskakun@gmail.com>
2015-04-15 11:51:45 +0300

    Some changes

--- app/controllers/controller_main.php
+++ app/controllers/controller_main.php
@@ -1,16 +1,8 @@
 <?php
 
-/**
- * class "Controller_Main"
- * Generate Main page
- */
 class Controller_Main extends Controller
 {
-    /**
-     * Constructor
-     *
-     * Get instance of Main Model and View Controller
-     */
+
     function __construct ($param)
     {
 
@@ -19,24 +11,11 @@ class Controller_Main extends Controller
 
     }
 
-    /**
-     * Action Index
-     *
-     * Generate HTML View
-     * @throws Exception
-     * @return mixed
-     */
     function action_index ()
     {
 
         $template = 'calendar_view.php';
-
-        /**
-         * @return array
-         */
-
         $data[] = $this->model->draw_calendar(4, 2015);
-
 
         if ( is_array( $data ) ) {
             return $this->view->generate( $template, 'template_view.php', $data );
