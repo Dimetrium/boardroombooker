29f46baaae741a09ec08843ae08860522d4e3f14
Dimetrium <diskakun@gmail.com>
2015-04-24 13:55:25 +0300

    Update template_view.php

--- app/views/template_view.php
+++ app/views/template_view.php
@@ -22,6 +22,6 @@
     </script>
 <script type="text/javascript" src="<?= JS ?>bootstrap.min.js"></script>
         <script type="text/javascript" src="<?= JS ?>calendar.js"></script>
-                <script type="text/javascript" src="<?= JS ?>datapicker.js"></script>
+        <script type="text/javascript" src="<?= JS ?>datapicker.js"></script>
 </body>
 </html>
