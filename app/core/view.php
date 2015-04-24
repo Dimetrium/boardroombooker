bb4680fb0f9fb489ad70ecbd7d2e7a7e5b4ffe6b
Dimetrium <diskakun@gmail.com>
2015-04-24 13:50:50 +0300

    Update view.php

--- app/core/view.php
+++ app/core/view.php
@@ -5,10 +5,31 @@ class View
 
     function generate ( $content_view, $template_view, $data = null )
     {
-//        foreach ( $data as $row ) {
-//            $data[ ] = $row;
-//        }
-//        $data = $data;
+
+// test
+
+$month = $data[ 'getDate' ][ 'mon' ];
+$year = $data[ 'getDate' ][ 'year' ];
+$boardrooms = $data[ 'boardrooms' ];
+/* select month control */
+for ( $x = 1; $x <= 12; $x++ ) {
+    $select_month_control .= '<option value="' . $x . '"' . ( $x != $month ? '' : ' selected="selected"' ) . '>' . date( 'F', mktime( 0, 0, 0, $x, 1, $data[ 'getDate' ][ 'year' ] ) ) . '</option>';
+}
+/* select year control */
+$year_range = 7;
+for ( $x = ( $data[ 'getDate' ][ 'year' ] - floor( $year_range / 2 ) ); $x <= ( $data[ 'getDate' ][ 'year' ] + floor( $year_range / 2 ) ); $x++ ) {
+    $select_year_control .= '<option value="' . $x . '"' . ( $x != $data[ 'getDate' ][ 'year' ] ? '' : ' selected="selected"' ) . '>' . $x . '</option>';
+}
+/* "next month" control */
+$next_month_link = ( $month != 12 ? $month + 1 : 1 ) . '/' . ( $month != 12 ? $year : $year + 1 );
+/* "previous month" control */
+$previous_month_link = ( $month != 1 ? $month - 1 : 12 ) . '/' . ( $month != 1 ? $year : $year - 1 );
+/* booardroom list */
+foreach ( $boardrooms as $room ) {
+    $rooms .= '<li class="list-group-item"><a href="main" onClick="DoPost(' . $room . ')">Boardroom # ' . $room . '</a></li>';
+}
+
+// test
         ob_start();
         include 'app/views/' . $template_view;
         $hz = ob_get_contents();
