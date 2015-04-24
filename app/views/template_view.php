306b8788e8357a389ff9c01c8c418c69a0ecb7ca
Dimetrium <diskakun@gmail.com>
2015-04-24 13:57:23 +0300

    Update template_view.php

--- app/views/template_view.php
+++ app/views/template_view.php
@@ -8,9 +8,8 @@
     <title><?= TITLE ?></title>
     <link rel="stylesheet" type="text/css" media="all" href="<?= CSS ?>bootstrap.min.css">
     <link rel="stylesheet" href="<?= CSS ?>CalendarBody.css"/>
-
+    <link rel="stylesheet" href="<?= CSS ?>datapicker.min.css"/>
     <script type="text/javascript" src="<?= JS ?>jquery-1.11.2.min.js"></script>
-
     <script type="text/javascript" src="http://scriptjava.net/source/scriptjava/scriptjava.js"></script>
 </head>
 <body>
@@ -20,8 +19,9 @@
             window.open("details", "_blank", "toolbar=no, location=no, scrollbars=yes, resizable=yes, top=500, left=500, width=600, height=400");
         }
     </script>
-<script type="text/javascript" src="<?= JS ?>bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?= JS ?>bootstrap.min.js"></script>
         <script type="text/javascript" src="<?= JS ?>calendar.js"></script>
         <script type="text/javascript" src="<?= JS ?>datapicker.js"></script>
+        <script src="<?= JS ?>bootstrap-datepicker.min.js"></script>
 </body>
 </html>
