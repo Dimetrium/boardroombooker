cc8de38ed70c618eae4625cf2173fa0d3f62ecbf
Dimetrium <diskakun@gmail.com>
2015-04-27 14:13:27 +0300

    Update header_view.php

--- app/views/header_view.php
+++ app/views/header_view.php
@@ -4,11 +4,12 @@
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="icon-toggle"></span>
             </button>
+                            <button type="submit" name="lang" method="post" value="ru">Ru</a>
+                <button type="submit" name="lang" method="post" value="en">En</a>
             <a class="navbar-brand" href="#">{{MAIN_H2}}</a>
         </div>
         <div class="navbar-collapse collapse">
-                            <button type="submit" name="lang" method="post" value="ru">Ru</a>
-                <button type="submit" name="lang" method="post" value="en">En</a>
+
             <ul class="nav navbar-nav navbar-right">
 
                 <li class="dropdown">
